4cc9ad2bd1081bec6a9ee32fa5505bfe3d08e0fc
Gil-Mendes-Carelli <gm.carelli@unesp.br>
2024-10-19 22:30:56 -0300

    Adding a starting notebook for data exploration

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from uinterface.dashboard import Dashboard
 
 def main():
     st.title("Visualizador de Dados Multidimensionais")
-    dashboard = Dashboard()
+    dashboard = Dashboard() 
     dashboard.load_data()
     # dashboard.display_visualizations()
 
